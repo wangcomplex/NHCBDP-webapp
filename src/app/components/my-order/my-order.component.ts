6f21ad9c2faeb23eeaff9d0037ed20c1f6f2a119
yangsyun <yangsy_2014@sina.com>
2017-11-23 17:16:13 +0800

    @yangsuyun:modify my-order

--- src/app/components/my-order/my-order.component.ts
+++ src/app/components/my-order/my-order.component.ts
@@ -7,9 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyOrderComponent implements OnInit {
 
+  flag00 = true;
+  flag01 = false;
+  flag02 = false;
+  flag03 = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  showContent(type) {
+    this.initFlag();
+    switch (type) {
+      case 0: {
+        this.flag00 = true;
+        break;
+      }
+      case 1: {
+        this.flag01 = true;
+        break;
+      }
+      case 2: {
+        this.flag02 = true;
+        break;
+      }
+      case 3: {
+        this.flag03 = true;
+        break;
+      }
+    }
+
+
+  }
+
+  initFlag() {
+    this.flag00 = false;
+    this.flag01 = false;
+    this.flag02 = false;
+    this.flag03 = false;
+  }
+
+  getStyle(flag) {
+    if (flag) {
+      return {
+        color: '#333'
+      };
+    }
+  }
 }
